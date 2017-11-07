387accfb0a4413e49fd1560cc5dd3ef64dd50964
Bert.Gibbons <bgibbons@udel.edu>
2017-11-07 11:30:23 -0500

    Getting ready for lab 5

---
+++ FlamingoBLL/src/test/java/pkgCore/TableTest.java
@@ -0,0 +1,18 @@
+package pkgCore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TableTest {
+
+	@Test
+	public void TableTest1() {
+		
+		//	TODO: Create 2 players.
+		//			Add them to the a Table
+		//			Ensure that each are added to the table
+		//			Remove each from the Table, ensure they are removed from the table.
+	}
+
+}
